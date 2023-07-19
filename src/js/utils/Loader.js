81c84391c58bff1b9bf42de55571725b3d59828c
Sean Kim <89332470+seanswim@users.noreply.github.com>
2023-07-20 08:52:33 +0900

    Update Loader.js

--- src/js/utils/Loader.js
+++ src/js/utils/Loader.js
@@ -1,10 +1,13 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import * as THREE from 'three'
+import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js'
 
 class Loader {
   constructor() {
     this.gltfLoader = new GLTFLoader()
     this.cubeTextureLoader = new THREE.CubeTextureLoader()
+    this.textureLoader = new THREE.TextureLoader()
+    this.EXRLoader = new EXRLoader()
   }
 }
 
