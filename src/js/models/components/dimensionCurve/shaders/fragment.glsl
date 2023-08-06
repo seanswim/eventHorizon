03349b8a77e597e54915fe6d5e5990edd3b34a5c
seanswim <swim1720@gmail.com>
2023-08-07 08:50:59 +0900

    update

--- src/js/models/components/dimensionCurve/shaders/fragment.glsl
+++ src/js/models/components/dimensionCurve/shaders/fragment.glsl
@@ -8,13 +8,15 @@ void main() {
 
   vec2 st = vec2(reflection.x, reflection.y);
 
-  float pct = distance(st, vec2(0.0));
-  float distortionStrength = ( 1.0 - pow(pct * 2.0, 5.0));
 
-  float s = sin(uTime * distortionStrength/10.0);
-  float c = cos(uTime * distortionStrength/10.0);
+  float pct = distance(st, vec2(0.0));
+  float distortionStrength = pow(pct * 2.0, 10.0);
+
+  float s = sin(uTime) * distortionStrength;
+  float c = cos(uTime) * distortionStrength;
   mat2 rotationMatrix = mat2(c, -s, s, c);
   vec2 rotatedOffset = rotationMatrix * st;
+
 
   vec3 color = vec3(0.0);
 
@@ -22,7 +24,13 @@ void main() {
   // rotatedOffset.x = distortionStrength;
   // rotatedOffset.y = distortionStrength;
 
-  color = textureCube(envMap, vec3(rotatedOffset, 1.0)).xyz;
+  // color = textureCube(envMap, vec3(rotatedOffset, 1.0)).xyz;
+
+  color = smoothstep(
+    color, 
+    textureCube(envMap, vec3(rotatedOffset, 1.0)).xyz, 
+    vec3 (distortionStrength)
+  );
 
 
   gl_FragColor = vec4(color, 1.0);
