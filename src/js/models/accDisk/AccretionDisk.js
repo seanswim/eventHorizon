6a8cf7099dd2579b965087d23a2f8a34c74cb93f
Sean Kim <swim1720@gmail.com>
2023-08-27 18:29:28 +0900

    update

--- src/js/models/accDisk/AccretionDisk.js
+++ src/js/models/accDisk/AccretionDisk.js
@@ -4,24 +4,40 @@ class AccretionDisk {
   constructor(world) {
     this.world = world
 
-    const radius = 3
+    const radius = 2
     const circlePoints = [];
     const segments = 100
+    const bendingAngle = THREE.MathUtils.degToRad(90); // Convert 30 degrees to radians
+    const tiltAngle = THREE.MathUtils.degToRad(45); // Convert 45 degrees to radians
 
     for (let i = 0; i <= segments; i++) {
-      let phi = (i / segments) * Math.PI * 2
-      
-      let x = Math.cos(phi) * radius
-      let y = Math.sin(phi) * radius
-      let z = 0
+      const phi = bendingAngle; // Fixed bending angle
+      // let phi = (i / segments) * Math.PI * 2
+      const theta = (i / segments) * Math.PI * 2;
 
-      // if (phi < Math.PI) {
-        let theta = Math.sin((i * Math.PI) / (segments/2)) * Math.PI * 2
-        x = radius * Math.sin(theta) * Math.cos(phi)
-        y = radius * Math.sin(theta) * Math.sin(phi)
-        z = radius * Math.cos(theta)
+      // let x = Math.cos(phi) * radius
+      // let y = Math.sin(phi) * radius
+      // let z = 0
+
+      // if (phi > Math.PI) {
+        // let theta = Math.PI/180 * (-((i/segments)*(i/segments-50)*(i/segments-100))*8/(50 * 100) + 80)
+        // let theta = Math.PI/180 * (-((i/segments)*(i/segments-50)*(i/segments-100))*8/(50 * 100) + 80)
+        // let theta = Math.PI/180 * ((-i * (i-100))/100 + 80)
+
+        // x = radius * Math.sin(theta) * Math.cos(phi)
+        // y = radius * Math.sin(theta) * Math.sin(phi)
+        // z = radius * Math.cos(theta)
       // }
-      circlePoints.push(new THREE.Vector3(x, y, z))
+
+      const x = radius * Math.sin(phi) * Math.cos(theta);
+      const y = radius * Math.sin(phi) * Math.sin(theta);
+      const z = radius * Math.cos(phi);
+
+      const rotatedY = y * Math.cos(tiltAngle) - z * Math.sin(tiltAngle);
+      const rotatedZ = y * Math.sin(tiltAngle) + z * Math.cos(tiltAngle);
+
+
+      circlePoints.push(new THREE.Vector3(x, rotatedY, rotatedZ))
     }
 
     const curve = new THREE.CatmullRomCurve3(circlePoints);
@@ -30,13 +46,12 @@ class AccretionDisk {
 
 
     this.geometry = new THREE.BufferGeometry().setFromPoints(points)
-    this.material = new THREE.LineBasicMaterial( { color: 0xffff00, linewidth: 1, side: THREE.DoubleSide } )
+    this.material = new THREE.LineBasicMaterial( { color: 0xffff00, linewidth: 10, side: THREE.DoubleSide } )
     
     this.axesHelper = new THREE.AxesHelper( 5 );
     
     this.mesh = new THREE.Line(this.geometry, this.material)
-    // this.mesh.rotateX( - Math.PI/180 * 70)
-    // this.mesh.rotateY( Math.PI/180 * 90)
+    // this.mesh.rotateX( - Math.PI/180 * 50)
     this.world.scene.add(this.mesh, this.axesHelper)
   }
 }
