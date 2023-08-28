a532148826c9a485ee0acdfe1fc63747af50eca4
Sean Kim <sean@fairlabs.io>
2023-08-28 19:01:03 +0900

    update

--- src/js/models/accDisk/AccretionDisk.js
+++ src/js/models/accDisk/AccretionDisk.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { getRandomFloat } from '@/js/utils/Utils'
+import FragmentShader from './shaders/fragment.glsl?raw'
+import VertexShader from './shaders/vertex.glsl?raw'
 
 class AccretionDisk {
   constructor(world) {
@@ -7,7 +9,7 @@ class AccretionDisk {
 
     this.disks = []
 
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 100; i++) {
       this.disks.push(...this.generateDisk(
         getRandomFloat(1,2),
         -80
@@ -20,11 +22,14 @@ class AccretionDisk {
   }
 
   generateDisk = (radius, tiltAngle, segments = 100, bendingAngle = THREE.MathUtils.degToRad(90)) => {
+
     const tiltAngle_top = THREE.MathUtils.degToRad(tiltAngle)
     const tiltAngle_bottom = THREE.MathUtils.degToRad(180 - Math.abs(tiltAngle))
     const circlePoints_top = [];
     const circlePoints_bottom = [];
+
     for (let i = 0; i <= segments; i++) {
+
       const phi = bendingAngle; 
       const theta = (i / segments) * Math.PI * 2;
 
@@ -36,27 +41,36 @@ class AccretionDisk {
       let z_top = z
       let y_bottom = y
       let z_bottom = z
+
       if (theta > Math.PI) {
         y_top = y * Math.cos(tiltAngle_top) - z * Math.sin(tiltAngle_top);
         z_top = y * Math.sin(tiltAngle_top) + z * Math.cos(tiltAngle_top);
       }
+      
       if (theta < Math.PI) {
-        y_top = y * Math.cos(tiltAngle_bottom) - z * Math.sin(tiltAngle_bottom);
-        z_top = y * Math.sin(tiltAngle_bottom) + z * Math.cos(tiltAngle_bottom);
+        y_bottom = y * Math.cos(tiltAngle_bottom) - z * Math.sin(tiltAngle_bottom);
+        z_bottom = y * Math.sin(tiltAngle_bottom) + z * Math.cos(tiltAngle_bottom);
       }
 
       circlePoints_top.push(new THREE.Vector3(x, y_top, z_top))
       circlePoints_bottom.push(new THREE.Vector3(x, y_bottom, z_bottom))
     }
+    
     const curve_top = new THREE.CatmullRomCurve3(circlePoints_top);
     const curve_bottom = new THREE.CatmullRomCurve3(circlePoints_bottom);
-    const points_top = curve_top.getPoints(500);
-    const points_bottom = curve_bottom.getPoints(500);
 
-    const geometry_top = new THREE.BufferGeometry().setFromPoints(points_top)
-    const geometry_bottom = new THREE.BufferGeometry().setFromPoints(points_bottom)
-    const material = new THREE.LineBasicMaterial( { color: 0xfffffff, linewidth: 5, side: THREE.DoubleSide } )
-    return [new THREE.Line(geometry_top, material), new THREE.Line(geometry_bottom, material)]
+    const geometry_top = new THREE.TubeGeometry(curve_top, segments, 0.01, 20, false)
+    const geometry_bottom = new THREE.TubeGeometry(curve_bottom, segments, 0.01, 20, false)
+
+    const material = new THREE.ShaderMaterial({
+      vertexShader: VertexShader,
+      fragmentShader: FragmentShader,
+    })
+
+    return [
+      new THREE.Line(geometry_top, material), 
+      new THREE.Line(geometry_bottom, material)
+    ]
   }
 }
 
