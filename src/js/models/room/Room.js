616b41cc4bc2dfb19dbba5733b9334480a71e53d
Sean Kim <sean@fairlabs.io>
2023-09-20 19:22:47 +0900

    update

--- src/js/models/room/Room.js
+++ src/js/models/room/Room.js
@@ -17,13 +17,13 @@ class Room {
         const model = gltf.scene;
         model.scale.set(10, 10, 10)
 
-        const position = new THREE.Vector3(x, y, z - 100); 
+        const position = new THREE.Vector3(x, y-25, z - 40); 
         model.position.copy(position);
         console.log(model)
 
         const light = new THREE.SpotLight(0xeeeeee, 0.7)
         const lighthelper = new THREE.SpotLightHelper(light); 
-        light.position.set(x, y + 40, z - 80)
+        light.position.set(x, y + 240, z - 80)
         this.world.scene.add(model, light, lighthelper)
       }
     )
@@ -32,9 +32,10 @@ class Room {
       'src/assets/icarus/scene.gltf',
       (gltf) => {
         const model = gltf.scene;
-        // model.scale.set(10, 10, 10)
 
-        const position = new THREE.Vector3(x, y, z - 100); 
+        const position = new THREE.Vector3(x + 5, y - 50, z + 140); 
+        model.rotation.x = Math.PI/180 * 225;
+
         model.position.copy(position);
 
         this.world.scene.add(model)
